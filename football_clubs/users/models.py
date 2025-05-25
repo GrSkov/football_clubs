65e96e55943fa55bf86ce5576167c1a159c5cd51
GrSkov <122297750+GrSkov@users.noreply.github.com>
2025-05-25 15:49:34 +0300

    Extend model Users

--- football_clubs/users/models.py
+++ football_clubs/users/models.py
@@ -5,4 +5,4 @@ from django.db import models
 # Create your models here.
 class Users(AbstractUser):
     photo = models.ImageField(upload_to='users/', blank=True, null=True, verbose_name="Фото профілю")
-    date_birth = models.DateTimeField(blank=True, null=True, verbose_name="Дата рождения")
+    date_birth = models.DateTimeField(blank=True, null=True, verbose_name="Дата народження")
